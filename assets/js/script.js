ea71455e76cad6f8f5a7667e52e068a319d406be
ldholl <lacy.d.holleman@gmail.com>
2022-09-12 14:30:25 -0400

    started adding cocktail fetch functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,36 @@
 //Global variables
 var keySearch = "";
 var movieObj
+var plot
 
 var posterContainerEl = document.querySelector("#posters");
 var modalEl = document.querySelector("#movie-modal");
 var moviePlotEl = document.querySelector(".movie-plot");
+var movieTitleEl = document.querySelector(".modal-card-title");
 
 //API keys: Lacy: Lacy Alt: k_ag013nc0 Jonathan: k_hm16evk8
 apiKey = "k_ag013nc0"
+//cocktailDB key 9973533
 
 //Event listener and function for button click **NOTE: might want to add alert for empty clicks
 $("#searchBtn").on("click", function (event){
     event.preventDefault();    
     keySearch = $(this).siblings("input").val();
-    console.log(keySearch);
+    
     getMovieId(keySearch);  
 });
 
-//Event listener for displaying modal when user clicks on a movie poster
-posterContainerEl.addEventListener("click", function(event) {
-    if (event.target.matches(".poster-img")) {
-        console.log("click");
-        imdbId = event.target.parentElement.id;
-        getMoviePlot(imdbId);
-        plotDrinkModal();
-    }
-});
 
 var getMovieId = function(){
     var searchUrl = "https://imdb-api.com/en/API/SearchMovie/" + apiKey + "/" + keySearch;
 
-    console.log(searchUrl);
-
     //Fetch request
     fetch(searchUrl).then(function(response){
-        console.log("response")
+     
         response.json().then(function(data){
-            console.log(data.results)
+           
            movieObj = data.results
-            console.log(movieObj)
+           
             showMovies();
         })
     })
@@ -55,8 +47,6 @@ var showMovies = function(){
 
     for (i = 0; i < movieObj.length; i ++){
         
-        
-
         //container for poster
         var posterEl = document.createElement("div");
         //id of poster is the id we need to pull the plot in getMoviePlot
@@ -84,26 +74,145 @@ var showMovies = function(){
 var getMoviePlot = function(imdbId){
     //format api url
     var apiUrl = "https://imdb-api.com/en/API/Wikipedia/k_hm16evk8/" + imdbId;
-    console.log(apiUrl)
+   
     //make request to url
     fetch(apiUrl).then(function(response){
         response.json().then(function(data){
-            console.log(data);
             // Sets movie's short plot as modal's text
             moviePlotEl.innerHTML = data.plotShort.plainText;
+            movieTitleEl.innerHTML = data.fullTitle;
+            plot = data.plotShort.plainText;
+            plot = plot.toLowerCase();
+            $(".modal").addClass("is-active");
         });
-    });
+    }
+    
+    );
     
 };
 
-var plotDrinkModal = function() {
-    console.log("poster clicked");
-    modalEl.style.display = "block";
-};
-
-// When the user clicks anywhere outside of the modal, the modal closes
-window.onclick = function(event) {
-    if (event.target == modalEl) {
-      modalEl.style.display = "none";
+//MODAL FUNCTIONS
+//listener for poster clicks
+posterContainerEl.addEventListener("click", function(event) {
+    if (event.target.matches(".poster-img")) {
+        imdbId = event.target.parentElement.id;
+        getMoviePlot(imdbId);
     }
-};
+});
+//listener for close clicks
+$(".modal-close").on("click", function(){
+    $(".modal").removeClass("is-active");
+})
+//listener for submit
+$(".modal-submit").on("click", function(event){
+    event.preventDefault();
+    $(".modal").removeClass("is-active");
+    location.href = "drink-movie.html"
+    checkKeywords();
+})
+
+var checkKeywords = function(){
+    console.log("checking keywords")
+    var mainIngr = "";
+    switch (true){
+        case plot.includes("japan" || "japanese" || "anime"):
+            mainIngr = "sake,";
+            break;
+        case plot.includes ("italy" || "italian"):
+            mainIngr = "amaretto,";
+            break;
+        case plot.includes ("russian" || "russia" || "soviet"):
+            mainIngr = "vodka,"
+            break;
+        case plot.includes ("french" || "france"):
+            mainIngr = "cognac,";
+            break;
+        case plot.includes("chinese,"):
+            //something
+            break
+        case plot.includes ("german"):
+            mainIngr = "schnapps,"
+            break;
+        case plot.includes("indian"):
+            mainIngr = "gin,"
+        case plot.includes ("portuguese" || "brazilian"):
+            mainIngr = "rum"                
+            break;
+        case plot.includes ("american" || "irish"):
+            mainIngr = "whiskey";
+            break;
+        case plot.includes ("british"):
+            mainIngr = "brandy,";
+            break;
+        default: break;
+    };
+
+    var secondIngr = "";
+    switch(true){
+        case plot.includes("action"):
+            secondIngr = "";
+            break;
+        case plot.includes("mystery"):
+            //something
+            break;
+        case plot.includes("crime"):
+            //something
+            break;
+        case plot.includes("fantasy"):
+            //something
+            break;
+        case plot.includes("science fiction"):
+            //something
+            break;
+        default: break;
+    }
+    getDrink();
+}
+
+
+///Add api fetch here
+var finalposter = $("#landing-poster")
+var drinkImgEl = $("#drink-img");
+var drinkName = $("#drink-name");
+var recipe = $("#recipe");
+
+
+var getDrink = function(mainIngr, secondIngr){
+console.log("fetching drinks")
+var mainIngr = "vodka,"
+var secondIngr = "orange_juice"
+var cocktailApi = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + mainIngr + secondIngr;
+console.log(cocktailApi);
+fetch(cocktailApi).then(function(response){
+    response.json().then(function(data){
+    console.log(data);
+    console.log(data.drinks.length)
+    if (data.drinks.length > 1){
+        var randNo = Math.floor(Math.random() * data.drinks.length + 1 )
+        console.log(data.drinks[0]);
+        drinkId = data.drinks[randNo].idDrink
+    }
+    else {
+        drinkId = data.drinks.idDrink;
+    }
+    setPage(drinkId);
+    })
+})
+}
+
+var setPage = function(drinkId){
+var cocktailIdUrl = "www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=" + drinkId;
+    fetch(cocktailIdUrl).then(function(response){
+    response.json().then(function(data){
+    console.log(data);
+    //finalposter.src = 
+    drinkImgEl.src =  data.strDrinkThumb;
+    drinkName.innerHTML = data.strDrink;
+    recipe.innerHtml = strInstructions;  
+
+        })
+    })
+}
+
+
+
